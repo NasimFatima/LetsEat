d85b6a522480e80a9e268e18b6c6c38c20cc860b
nasimfatima <fatimawaheed143@gmail.com>
2021-01-18 18:52:25 +0500

    remove google login url

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.urls import re_path
 from rest_auth.registration.views import VerifyEmailView
 
-from .views import (CustomRegisterView, GoogleView,
+from .views import (CustomRegisterView,
                     LoginView, UserViewSet, GroupViewSet, LogoutView)
 from rest_framework import routers
 
@@ -15,7 +15,6 @@ urlpatterns = [
     url(r'registration/', CustomRegisterView.as_view(),
         name='my_custom_registration'),
     url(r'login/', LoginView.as_view(), name='my_login_view'),
-    url(r'google/', GoogleView.as_view(), name='google'),
     url(r'logout/', LogoutView.as_view(), name='my_logout_view')
 ]
 
